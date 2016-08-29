9213909940db02444a4366f7b3067d4ca0d13378
Hesto <losiakp@gmail.com>
2016-08-29 10:13:20 +0200

    fix 6

--- src/Commands/InstallGuardCommand.php
+++ src/Commands/InstallGuardCommand.php
@@ -13,7 +13,7 @@ class InstallGuardCommand extends ReplaceContentCommand
      *
      * @var string
      */
-    protected $name = 'multi-auth:guard';
+    protected $name = 'multi-auth:provider';
 
     /**
      * The console command description.
