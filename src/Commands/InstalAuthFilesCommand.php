2922a1f154407e2856a8d570a892c742f934fe7b
Hesto <losiakp@gmail.com>
2016-08-29 14:32:31 +0200

    fix 21

---
+++ src/Commands/InstalAuthFilesCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Hesto\MultiAuth\Commands;
+
+use Hesto\Core\Commands\InstallFilesCommand;
+use Symfony\Component\Console\Input\InputOption;
+
+
+class InstallAuthFilesCommand extends InstallFilesCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'multi-auth:settings';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Install settings in files';
+
+    /**
+     * Get the destination path.
+     *
+     * @return string
+     */
+    public function getFiles()
+    {
+        $name = $this->getNameInput();
+
+        return [
+            'guard' => [
+                'path' => '/routes/' . $name .'.php',
+                'stub' => __DIR__ . '/../stubs/routes/routes.stub',
+            ],
+        ];
+    }
+}
